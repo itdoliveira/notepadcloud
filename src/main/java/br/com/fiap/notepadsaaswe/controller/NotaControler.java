11f87468082d9e3ce04f8735ef8dfa0d27ea0e14
logonrm <logonrm@fiap.com.br>
2017-08-15 20:37:08 -0300

    Adicionado novo metodo

--- src/main/java/br/com/fiap/notepadsaaswe/controller/NotaControler.java
+++ src/main/java/br/com/fiap/notepadsaaswe/controller/NotaControler.java
@@ -2,30 +2,34 @@ package br.com.fiap.notepadsaaswe.controller;
 
 import java.util.List;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import br.com.fiap.notepadsaaswe.model.Nota;
 import br.com.fiap.notepadsaaswe.repository.NotaRepository;
 
 @RestController
+@CrossOrigin
+@RequestMapping(value = "/nota")
 public class NotaControler {
 
-	private NotaRepository notaRepository;
+    private NotaRepository notaRepository;
 
-	public NotaControler(NotaRepository notaRepository) {
-		this.notaRepository = notaRepository;
-	}
+    public NotaControler(NotaRepository notaRepository) {
+        this.notaRepository = notaRepository;
+    }
 
-	@GetMapping(value= "/")
-	public List<Nota> findAll(){
-		return notaRepository.findAll();
-	}
-	
-	@PostMapping(value= "/")
-	public void save(@RequestBody Nota nota){
-		notaRepository.save(nota);
-	}
+    @GetMapping(value = "/")
+    private List<Nota> findAll() {
+        return notaRepository.findAll();
+    }
+
+    @GetMapping(value = "titulo/{titulo}")
+    private List<Nota> findByTitulo(@PathVariable(value = "titulo") String q) {
+        return notaRepository.findByTitulo(q);
+    }
+
+    @PostMapping(value = "/")
+    public void save(@RequestBody Nota nota) {
+        notaRepository.save(nota);
+    }
 }
--- src/main/java/br/com/fiap/notepadsaaswe/repository/NotaRepository.java
+++ src/main/java/br/com/fiap/notepadsaaswe/repository/NotaRepository.java
@@ -4,6 +4,9 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 
 import br.com.fiap.notepadsaaswe.model.Nota;
 
+import java.util.List;
+
 public interface NotaRepository extends MongoRepository<Nota, String>{
 
+    List<Nota> findByTitulo(String titulo);
 }
